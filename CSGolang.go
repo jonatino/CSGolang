9fdffa91208a2cb9bf38a8c3d06d53870eef5453
Jonatino <jonathanbeaudoin1996@gmail.com>
2018-03-01 00:18:29 -0500

    Finished getting process id by process name

--- CSGolang.go
+++ CSGolang.go
@@ -2,23 +2,50 @@ package main
 
 import (
 	"./win32"
+	"errors"
 	"fmt"
-	"syscall"
 	"unsafe"
 )
 
 func main() {
-	processByName()
+	pid, err := processByName("explorer.exe")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println("Your process id is", pid)
 }
 
-func processByName() {
+func processByName(name string) (uint32, error) {
 	var entry win32.PROCESSENTRY32
 	entry.Size = uint32(unsafe.Sizeof(entry))
 
 	snap := win32.CreateToolhelp32Snapshot(win32.TH32CS_SNAPALL, 0)
-	fmt.Println(snap)
 
-	for win32.Process32Next(snap, &entry) {
-		fmt.Println(entry.ProcessID, syscall.UTF16ToString(entry.SzExeFile[:]), entry.SzExeFile[:])
+	if !win32.Process32First(snap, &entry) {
+		return 0, errors.New(fmt.Sprintf("Process %s not found!", name))
 	}
+
+	for ok := true; ok; ok = win32.Process32Next(snap, &entry) { //Because why would GO have a fucking do while loop
+		processName := GetProcessName(&entry)
+		if processName == name {
+			return entry.ProcessID, nil
+		}
+	}
+
+	return 0, errors.New(fmt.Sprintf("Process %s not found!", name))
+}
+
+func GetProcessName(entry *win32.PROCESSENTRY32) string {
+	return string(entry.SzExeFile[:parseTerminatedString(entry.SzExeFile[:])])
+}
+
+func parseTerminatedString(n []byte) int {
+	for i := 0; i < len(n); i++ {
+		if n[i] == 0 {
+			return i
+		}
+	}
+	return len(n)
 }
